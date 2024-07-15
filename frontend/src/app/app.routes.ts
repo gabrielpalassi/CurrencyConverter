974338e36b69ed1826e8ee66f8f8cb783608e58f
Gabriel Palassi <gabrielpalassi@hotmail.com>
2024-07-15 04:04:05 -0300

    feat(about): add about page's component and route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { EmptyPageComponent } from './pages/empty-page/empty-page.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
   },
   {
     path: '**',
