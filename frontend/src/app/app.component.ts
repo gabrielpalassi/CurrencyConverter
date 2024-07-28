07c1c2da6fa10ec0eab3de7df0f0dcc29b7a2e97
Gabriel Palassi <gabrielpalassi@hotmail.com>
2024-07-28 08:58:25 -0300

    feat(error-modal): add error modal component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ErrorModal } from './shared/components/error-modal/error-modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, HeaderComponent],
+  imports: [RouterModule, HeaderComponent, ErrorModal],
   templateUrl: './app.component.html',
 })
 export class AppComponent {}
---
+++ frontend/src/app/shared/components/error-modal/error-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'error-modal',
+  standalone: true,
+  templateUrl: './error-modal.component.html',
+})
+export class ErrorModal {}
