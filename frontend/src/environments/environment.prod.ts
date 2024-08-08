4225357bd22c4d84062aaf383dfa5c178446bdf3
Gabriel Palassi <gabrielpalassi@hotmail.com>
2024-08-08 11:34:29 -0300

    chore(vercel): change api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import Environment from '../app/shared/interfaces/environment.interface';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'https://currencyconverter-backend-lbp1.onrender.com',
+  apiUrl: 'https://currencyconverter-backend.vercel.app',
 };
