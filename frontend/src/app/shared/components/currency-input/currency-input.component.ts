f55c8eaf752bf57b90b57988441fc311a2d2238d
Gabriel Palassi <gabrielpalassi@hotmail.com>
2024-10-25 06:52:41 +0200

    refactor(symbols): remove div for symbols and instead use buitin currencyMask option

--- frontend/src/app/shared/components/currency-input/currency-input.component.ts
+++ frontend/src/app/shared/components/currency-input/currency-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, input, model, output, viewChild } from '@angular/core';
+import { Component, input, model, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgxCurrencyDirective } from 'ngx-currency';
 
@@ -16,23 +16,4 @@ export class CurrencyInputComponent {
   amount = model<number>();
   error = model<boolean>(false);
   enterPressed = output<void>();
-
-  // Reference symbolDiv and input elements
-  symbolDiv = viewChild.required<ElementRef>('symbolDiv');
-  amountInput = viewChild.required<ElementRef>('amountInput');
-
-  // Set inputLeftPadding every time the view is checked
-  ngAfterViewChecked() {
-    const offsetWidthInPx = this.symbolDiv().nativeElement.offsetWidth;
-    const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
-    const offsetWidthInRem = offsetWidthInPx / rootFontSize;
-    const inputLeftPadding = this.symbolDiv().nativeElement.offsetWidth ? offsetWidthInRem + 0.1875 + 'rem' : '0.5rem';
-    this.amountInput().nativeElement.style.paddingLeft = inputLeftPadding;
-  }
-
-  // Event handler for when the input amount changes
-  onModelChange(event: number) {
-    this.amount.set(event);
-    this.error.set(false);
-  }
 }
